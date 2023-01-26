43d64967e6f549ffeeb6e8adc91f0bf6ed16944d
NFelipe <nfelipe.gonzaalez.96@gmail.com>
2023-01-26 14:53:51 -0300

    v 0.0 premodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormacionComponent } from './components/formacion/formacion.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PortadaComponent } from './components/portada/portada.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    FormacionComponent,
+    NavbarComponent,
+    PortadaComponent,
+    ProyectosComponent,
+    SobremiComponent,
+    TecnologiasComponent
   ],
   imports: [
     BrowserModule
